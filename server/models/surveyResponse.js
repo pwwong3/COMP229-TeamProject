a6d3eaa68cdd3e2810c4ee3e93f64d6f935c284f
Wong Pak Wah <pwwong3@users.noreply.github.com>
2022-11-11 18:50:38 -0500

    COMP229-2 Data model created

---
+++ server/models/surveyResponse.js
@@ -0,0 +1,44 @@
+/*******************************
+ * File name: surveyResponse.js
+ * Name: Pak Wah Wong
+ * StudentID: 301255741
+ * Date: 2022.11.11
+*******************************/
+
+// require modules for the Survey Response model
+let mongoose = require('mongoose');
+
+let SurveyResponse = mongoose.Schema({
+    surveyId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: 'Survey ID is required'
+    },
+    responses: [{
+        questionId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: 'Question ID is required'
+        },
+        response: {
+            type: String,
+            default: '',
+            trim: true
+        }
+    }],
+    userId: {
+        type: Schema.Types.ObjectId,
+        default: '636ede98c28da25cc52e4291'
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    update: {
+        type: Date,
+        default: Date.now
+    }
+},
+{
+    collection: "surveyResponses"
+});
+
+ module.exports.SurveyResponse = mongoose.model('SurveyResponse', SurveyResponse);
